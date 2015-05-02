d265524f8b49ace1a56d35d3dc420e9bed359992
Farhad Safarov <farhad.safarov@gmail.com>
2015-05-02 14:38:09 +0300

    translations

---
+++ AzerdictAssetsBundle.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This file is part of the azerdict.com package.
+ *
+ * (c) azerdict.com <http://azerdict.com>
+ * @author Farhad Safarov <http://ferhad.in>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Azerdict\AssetsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AzerdictAssetsBundle extends Bundle
+{
+}
